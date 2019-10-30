b174df32182e6a70af98f637a012e3825ac4c917
rong.zhao <rong.zhao@tellyes.com>
2019-10-30 14:39:37 +0800

    测试

--- spring-activemq/src/main/test/com/jugheadzhou/jms/QueueConsumerTest.java
+++ spring-activemq/src/main/test/com/jugheadzhou/jms/QueueConsumerTest.java
@@ -1,9 +1,16 @@
 package com.jugheadzhou.jms;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;
 import javax.jms.Message;
 import javax.jms.TextMessage;
 
@@ -12,12 +19,18 @@ import javax.jms.TextMessage;
  * @author: aishu
  * @create: 2019-10-24 13:39
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:config/spring-config.xml")
 public class QueueConsumerTest {
-    public static void main(String[] args) throws Exception{
+    @Autowired
+    @Qualifier(value = "jmsQueueTemplate")
+    private JmsTemplate jmsTemplate;
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:config/spring-config.xml");
-
-        JmsTemplate jmsTemplate = (JmsTemplate) applicationContext.getBean("jmsQueueTemplate");
+    @Test
+    public void receive() throws Exception{
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:config/spring-config.xml");
+//
+//        JmsTemplate jmsTemplate = (JmsTemplate) applicationContext.getBean("jmsQueueTemplate");
         TextMessage message = (TextMessage)jmsTemplate.receive("scoreQueueDestination");
         System.out.println("接收到消息："+message.getText());
     }
--- spring-activemq/src/main/test/com/jugheadzhou/jms/QueueProducerTest.java
+++ spring-activemq/src/main/test/com/jugheadzhou/jms/QueueProducerTest.java
@@ -1,11 +1,18 @@
 package com.jugheadzhou.jms;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractXmlApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
@@ -16,13 +23,19 @@ import javax.jms.TextMessage;
  * @author: aishu
  * @create: 2019-10-24 09:45
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:config/spring-config.xml")
 public class QueueProducerTest {
+    @Resource
+    @Qualifier(value = "jmsQueueTemplate")
+    private JmsTemplate jmsTemplate;
 
-    public static void main(String[] args) {
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:config/spring-config.xml");
-
-        JmsTemplate jmsTemplate = (JmsTemplate) applicationContext.getBean("jmsQueueTemplate");
+    @Test
+    public void send(){
+        System.out.println(jmsTemplate);
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:config/spring-config.xml");
+//        JmsTemplate jmsTemplate = (JmsTemplate) applicationContext.getBean("jmsQueueTemplate");
         jmsTemplate.send(new MessageCreator() {
             @Override
             public Message createMessage(Session session) throws JMSException {
